5a9c593c09aa464b4542010f150b6b95d9fee157
gracebir <grace.devolop@gmail.com>
2023-11-27 21:04:00 +0200

    login controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -3,8 +3,9 @@
 import { Request, Response } from "express";
 import prisma from "../utils/seed";
 import bcrypt from "bcryptjs";
-import { registerSchema } from "../validations/user.validation";
+import { loginSchema, registerSchema } from "../validations/user.validation";
 import Joi from "joi";
+import { signInJwt } from "../utils/jwt";
 
 const getUser = async (email: string) => {
     const user = await prisma.user.findUnique({
@@ -42,6 +43,38 @@ export default {
                 id: newUser.id,
                 name: newUser.name,
                 email: newUser.email,
+            });
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    },
+    login: async (req: Request, res: Response) => {
+        const { email, password } = req.body;
+        const validate = Joi.validate(req.body, loginSchema);
+        if (validate.error)
+            return res
+                .status(400)
+                .json({ error: validate.error.details[0].message });
+        try {
+            const user = await getUser(email);
+            if (!user)
+                return res.status(400).json({ message: "User does not exist" });
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch)
+                return res.status(400).json({ message: "Incorrect password" });
+            const accessToken = await signInJwt(
+                { id: user.id, email: user.email },
+                "1d"
+            );
+            res.cookie("accessToken", accessToken, {
+                maxAge: 300000,
+                httpOnly: true,
+            });
+            return res.status(200).json({
+                accessToken,
+                id: user.id,
+                email: user.email,
+                name: user.name,
             });
         } catch (error) {
             res.status(500).json(error);
