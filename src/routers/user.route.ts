769d572ae6286b1f800bd87c4ce9cc0fdba83550
gracebir <grace.devolop@gmail.com>
2023-11-27 21:04:49 +0200

    user router

--- src/routers/user.route.ts
+++ src/routers/user.route.ts
@@ -6,5 +6,6 @@ import userController from "../controllers/user.controller";
 const router = Router();
 
 router.post("/register", userController.registerUser);
+router.post("/signin", userController.login);
 
 export default router;
