e62fee14e3000d773becbced23796d1115532177
gracebir <grace.devolop@gmail.com>
2023-11-28 09:06:09 +0200

    deleting user

--- src/routers/user.route.ts
+++ src/routers/user.route.ts
@@ -7,5 +7,7 @@ const router = Router();
 
 router.post("/register", userController.registerUser);
 router.post("/signin", userController.login);
+router.delete("/signout", userController.logout);
+router.delete("/delete/:id", userController.deleteUser);
 
 export default router;
